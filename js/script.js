f5830bda79767818675b0f828d0cd34968eea588
Benjamín Navarro <be.navarroa@duocuc.cl>
2021-05-10 01:29:09 -0400

    Corrección validación contacto

--- js/script.js
+++ js/script.js
@@ -107,3 +107,40 @@ $.getJSON('https://mindicador.cl/api', function(data){
     $("#dolar").html('$' + data.dolar.valor);
     $("#fechadolar").html(today);
 });
+
+
+$(document).ready(function() {
+    
+    //validar campos de formulario
+    $("#bEnviar").click(function(){
+        var nombre = $("#itNombre").val();
+        var email = $("#itEmail").val();
+        var celular = $("#itCelular").val();
+        var mensaje = $("#itMensaje").val();
+        
+        if (nombre =="" || nombre.length < 2){
+            $("#mensaje1").fadeIn();
+            return false;
+        }else{
+            $("#mensaje1").fadeOut();
+            if (email =="" || !expr.test(email)){
+                $("#mensaje2").fadeIn();
+                return false;
+            }else{
+                $("#mensaje2").fadeOut();
+                if (celular =="" || celular.length < 8){
+                    $("#mensaje3").fadeIn();
+                    return false;
+                }else{
+                    $("#mensaje3").fadeOut();
+                    if (mensaje ==""){
+                        $("#mensaje4").fadeIn();
+                        return false;
+                    }else{
+                        $("#mensaje4").fadeOut();
+                    }
+                }
+            }
+        }
+    });
+});
